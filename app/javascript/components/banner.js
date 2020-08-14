5b45974ab79667baac7e2a1c7ef94c40afc9d417
Anthony You <you.anthony@yahoo.com>
2020-08-14 16:07:13 +0200

    added style

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Change your life", "Learn to make awesome cocktails."],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
